d4714fbf20141f7d7add70d7d1ecdb96e9b08d5f
Pablo Lires <pablolires@gmail.com>
2025-10-10 18:11:55 -0300

    Implementacion de nuevos componentes de producto

--- src/pages/ProductWallPanel.jsx
+++ src/pages/ProductWallPanel.jsx
@@ -3,6 +3,9 @@ import ColorsProduct from '@/components/commons/ColorsProduct.jsx'
 import GalleryProduct from '@/components/commons/GalleryProduct.jsx'
 import MedidasProduct from '@/components/commons/MedidasProduct.jsx'
 import ComparacionProduct from '@/components/commons/ComparacionProduct.jsx'
+import AccessoriesProduct from '@/components/commons/AccessoriesProduct.jsx'
+import TechnicalSheetProduct from '@/components/commons/TechnicalSheetProduct.jsx'
+import QuoteForm from '@/components/commons/QuoteForm.jsx'
 
 import product from '@/data/wall-panel.json'
 
@@ -11,23 +14,38 @@ import './product.css'
 const ProductWallPanel = () => {
   return (
     <main className='product'>
-      <HeaderProduct product={product['header'][0]} />
+      <HeaderProduct product={product.header[0]} />
       <section className='contentColors container'>
         <div className='row'>
           <div className='col-md-12'>
             <h3>Colores Disponibles</h3>
           </div>
         </div>
-
         <div className='row'>
           <div className='col-md-12'>
-            <ColorsProduct product={product['colores']} />
+            <ColorsProduct product={product.colores} />
           </div>
         </div>
+        <GalleryProduct product={product.gallery} />
+        <MedidasProduct product={product.medidas} />
+        <ComparacionProduct product={product.comparacion} />
+        {product.accessories && (
+          <AccessoriesProduct product={product.accessories} />
+        )}
+        <TechnicalSheetProduct
+          data={product.technicalSheet.data}
+          ventajas={product.technicalSheet.ventajas}
+        />
 
-        <GalleryProduct product={product['gallery']} />
-        <MedidasProduct product={product['medidas']} />
-        <ComparacionProduct product={product['comparacion']} />
+        <section className='quoteFormProduct container'>
+          <div className='row'>
+            <div className='col-md-12 col-lg-8 offset-lg-2'>
+              <QuoteForm
+                recaptchaSiteKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
+              />
+            </div>
+          </div>
+        </section>
       </section>
     </main>
   )
