a060b62a678d2cee00411c2daae1ff1e66057955
Pablo Lires <pablolires@gmail.com>
2025-10-10 18:10:29 -0300

    Creacion de AccessoriesProduct.jsx

---
+++ src/components/commons/AccessoriesProduct.jsx
@@ -0,0 +1,32 @@
+import { getImageURL } from '@/utils/dataUtils.js'
+
+import styles from './accessories-product.module.css'
+
+const AccessoriesProduct = ({ product }) => {
+  return (
+    <section
+      className={`accessoriesProduct container ${styles.accessoriesProduct}`}
+    >
+      <div className='row'>
+        <div className='col-md-12'>
+          <h3 className='latoBoldItalic'>Accesorios</h3>
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-md-6'>
+          <img
+            className='img-fluid'
+            src={getImageURL(product[0].img_src)}
+            alt='accesorios del producto'
+          />
+        </div>
+        <div className='col-md-6'>
+          <p>{product[0].description}</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AccessoriesProduct
