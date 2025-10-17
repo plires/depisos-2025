9db99415481d3030e3b0fe310e538bdacf155581
Pablo Lires <pablolires@gmail.com>
2025-10-17 17:54:09 -0300

    Update Sustentabilidad.jsx. Se agrega efecto de scroll (Aos)

--- src/pages/Sustentabilidad.jsx
+++ src/pages/Sustentabilidad.jsx
@@ -19,22 +19,22 @@ const Profesionales = () => {
       <section className='contentSustentabilidad container'>
         <div className='row'>
           <div className='col-lg-10 offset-lg-1 contentData'>
-            <p>
+            <p data-aos='fade-up'>
               En Depisos creemos que cada elección que hacemos hoy define el
               mundo en el que vamos a vivir mañana. Por eso, trabajamos con
               productos y procesos que priorizan la responsabilidad ambiental
               sin resignar calidad ni diseño.
             </p>
-            <p>
+            <p data-aos='fade-up'>
               Utilizamos materiales reciclables y de larga duración, que reducen
               la generación de residuos. Apostamos a tecnologías de bajo impacto
               ambiental.
             </p>
-            <p>
+            <p data-aos='fade-up'>
               Impulsamos soluciones que favorecen la eficiencia energética en
               los espacios, aportando confort térmico y durabilidad.
             </p>
-            <p>
+            <p data-aos='fade-up'>
               Porque nuestro compromiso no es solo con tus proyectos, sino
               también con el planeta que compartimos.
             </p>
@@ -44,6 +44,7 @@ const Profesionales = () => {
         <div className='row'>
           <div className='col-md-12'>
             <img
+              data-aos='fade-up'
               className='img-fluid'
               src={content}
               alt='contenido sustentabilidad'
