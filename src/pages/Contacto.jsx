5ed30157eea7e1df6734c5a6983b45d589344a97
Pablo Lires <pablolires@gmail.com>
2025-10-23 18:08:15 -0300

    ajustes en page contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -2,6 +2,11 @@ import HeroSection from '@/components/commons/HeroSection'
 import QuoteForm from '@/components/commons/QuoteForm.jsx'
 import GoogleMap from '@/components/commons/GoogleMap.jsx'
 
+import { CiLocationOn } from 'react-icons/ci'
+import { IoIosPhonePortrait } from 'react-icons/io'
+import { MdOutlineWatchLater } from 'react-icons/md'
+import { HiOutlineMail } from 'react-icons/hi'
+
 import heroDesktop from '@/assets/img/header-contacto-desktop.webp'
 import heroMobile from '@/assets/img/header-contacto-mobile.webp'
 
@@ -14,21 +19,39 @@ const Contacto = () => {
         heroDesktop={heroDesktop}
         heroMobile={heroMobile}
         title='contactate <br />con nosotros'
+        extraClass='contentContact'
       />
+
       <section className='container'>
         <div data-aos='fade-up' className='row'>
-          <div className='col-md-4'>
-            <h4>Showroom San Martín</h4>
+          <div className='col-md-4 contentData'>
+            <h4 className='latoBoldItalic'>Showroom San Martín</h4>
             <p>
+              <span>
+                <CiLocationOn />
+              </span>{' '}
               Dirección: Calle 56 n°4575, San Martín, Buenos Aires, Argentina.
             </p>
-            <p>Teléfono: (+54) 11 6379 0009</p>
             <p>
+              <span>
+                <IoIosPhonePortrait />
+              </span>{' '}
+              Teléfono: (+54) 11 6379 0009
+            </p>
+            <p>
+              <span>
+                <MdOutlineWatchLater />
+              </span>{' '}
               Horarios: Lunes a Viernes 9:00 - 18:00 hrs. Sábado 9:30 13:30 hrs.
             </p>
-            <p>Email: info@depisos.com</p>
+            <p>
+              <span>
+                <HiOutlineMail />
+              </span>{' '}
+              Email: info@depisos.com
+            </p>
           </div>
-          <div className='col-md-7'>
+          <div className='col-md-8 col-lg-7 offset-lg-1 contentForm'>
             <QuoteForm
               recaptchaSiteKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
               type='contacto'
@@ -36,7 +59,7 @@ const Contacto = () => {
           </div>
         </div>
 
-        <div className='row'>
+        <div className='row contentMap'>
           <div className='col-md-12'>
             <GoogleMap
               src={import.meta.env.VITE_GOOGLE_MAP}
