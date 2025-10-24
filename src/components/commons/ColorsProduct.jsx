a12ca13dbb3752808dc0f6623d9a47839566522d
Pablo Lires <pablo@librecomunicacion.net>
2025-10-24 11:04:41 -0300

    Roll back del componente ColorsProduct.jsx

--- src/components/commons/ColorsProduct.jsx
+++ src/components/commons/ColorsProduct.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-import Slider from 'react-slick'
 import { getImageURL } from '@/utils/dataUtils.js'
 import Modal from '@/components/commons/ModalColorProduct.jsx'
 
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import styles from './colors-product.module.css'
 
 const ColorsProduct = ({ product }) => {
@@ -20,80 +17,29 @@ const ColorsProduct = ({ product }) => {
     setSelectedProduct(null)
   }
 
-  // Configuración del carrusel Slick responsivo
-  const sliderSettings = {
-    dots: false,
-    infinite: true,
-    // autoPlay: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    arrows: true,
-    swipeToSlide: true,
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  }
-
   return (
     <>
       <div
         data-aos='fade-up'
         className={[
-          styles.contentColors,
-          'contentColors',
-          extraClass && (styles[extraClass] ?? extraClass),
+          styles.contentColors, // CSS Module
+          'contentColors', // clase global
+          extraClass && (styles[extraClass] ?? extraClass), // usa styles[extraClass] si existe en el módulo; si no, la agrega como global
         ]
           .filter(Boolean)
           .join(' ')}
       >
-        <Slider {...sliderSettings} className={styles.colorSlider}>
-          {product.map(item => (
-            <div key={item.id} className={styles.slideWrapper}>
-              <article
-                className={styles.color}
-                onClick={() => handleOpenModal(item)}
-              >
-                <h4>{item.title}</h4>
-                <img src={getImageURL(item.img_src)} alt={item.title} />
-              </article>
-            </div>
-          ))}
-        </Slider>
+        {product.map(item => (
+          <article key={item.id} className={styles.color}>
+            <button
+              onClick={() => handleOpenModal(item)}
+              className={styles.colorButton}
+            >
+              <h4>{item.title}</h4>
+              <img src={getImageURL(item.img_src)} alt={item.title} />
+            </button>
+          </article>
+        ))}
       </div>
 
       <Modal isOpen={!!selectedProduct} onClose={handleCloseModal}>
