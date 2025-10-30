44ab537a046dddeef7ff1a2fd8f8f04b5185e707
Pablo Lires <pablolires@gmail.com>
2025-10-30 17:52:31 -0300

    Creacion de Page "ProductVinylPrinter"

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import ProductSiding from '@/pages/ProductSiding.jsx'
 import ProductFlatPanel from '@/pages/ProductFlatPanel.jsx'
 import ProductCielorrasoFoliado from '@/pages/ProductCielorrasoFoliado.jsx'
 import ProductCielorrasoPrinter from '@/pages/ProductCielorrasoPrinter.jsx'
+import ProductVinylPrinter from '@/pages/ProductVinylPrinter.jsx'
 import ProductVinylPlenos from '@/pages/ProductVinylPlenos.jsx'
 import Profesionales from '@/pages/Profesionales.jsx'
 import Sustentabilidad from '@/pages/Sustentabilidad.jsx'
@@ -49,6 +50,10 @@ function App() {
             <Route
               path='/productos/cielorraso/foliados'
               element={<ProductCielorrasoFoliado />}
+            />
+            <Route
+              path='/productos/vinyl-panel/printer'
+              element={<ProductVinylPrinter />}
             />
             <Route
               path='/productos/vinyl-panel/plenos'
