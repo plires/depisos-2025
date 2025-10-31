62255bf9b14a07c18664e765759db5f6ebe7149f
Pablo Lires <pablolires@gmail.com>
2025-10-31 10:24:07 -0300

    Suma de estilos para OtherLinesProduct.jsx

--- src/components/commons/OtherLinesProduct.jsx
+++ src/components/commons/OtherLinesProduct.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import ButtonApp from '@/components/commons/ButtonApp.jsx'
 
 import styles from './other-lines-product-product.module.css'
@@ -7,29 +6,27 @@ const OtherLinesProduct = ({ product }) => {
   const extraClass = product?.[0]?.extra_class
 
   return (
-    <>
-      <div
-        className={[
-          styles.contentOtherLines, // CSS Module
-          'contentOtherLines', // clase global
-          extraClass && (styles[extraClass] ?? extraClass), // usa styles[extraClass] si existe en el módulo; si no, la agrega como global
-        ]
-          .filter(Boolean)
-          .join(' ')}
-      >
-        {product.map(item => (
-          <ButtonApp
-            key={item.id}
-            data-aos='fade-up'
-            to={item.src}
-            variant='primary'
-            size='md'
-          >
-            {item.linea}
-          </ButtonApp>
-        ))}
-      </div>
-    </>
+    <div
+      className={[
+        styles.contentOtherLines, // CSS Module
+        'contentOtherLines', // clase global
+        extraClass && (styles[extraClass] ?? extraClass), // usa styles[extraClass] si existe en el módulo; si no, la agrega como global
+      ]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      {product.map(item => (
+        <ButtonApp
+          key={item.id}
+          data-aos='fade-up'
+          to={item.src}
+          variant='primary'
+          size='md'
+        >
+          {item.linea}
+        </ButtonApp>
+      ))}
+    </div>
   )
 }
 
